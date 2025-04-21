67c5e0942d7b5fd76f2b9fc6e8446914934fc9d2
aji883 <ajilltan@gmail.com>
2025-04-21 15:16:34 +0700

    tugas praktikum

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import Greeting from './components/Greeting';
 import ProductList from './components/ProductList';
+import Mahasiswa from './components/Mahasiswa';
+import PostList from './components/PostList';
+
 function App() {
  return (
  <div>
  <h1>React Frontend - Pertemuan 9</h1>
  <Greeting name="Mahasiswa" />
  <ProductList /> 
+ <Mahasiswa nama="Haidar" npm="12345" prodi="TeknikInformatika" />
+ <PostList />
  </div>
  );
 }
